6e1caf053c199585fbc0abc392d7586b38a6b348
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2024-10-13 10:47:19 -0700

    integration

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 
 import os
 import shutil
-import uuid
 import tempfile
+import uuid
 
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse, JSONResponse
 
 from inference import process_video
 
@@ -26,38 +26,30 @@ app.add_middleware(
 def root():
     return {"status": "API is running"}
 
+
 @app.post("/process")
-async def process_file(file: UploadFile = File(...)):
-    # Validate file type
-    if not file.filename.lower().endswith('.mp4'):
-        raise HTTPException(status_code=400, detail="Only .mp4 files are supported.")
+def process(file: UploadFile = File(...)):
+    file_name = "user_videos_processed/" + file.filename.rsplit(".", 1)[0] + ".npy"
+    with open(file.filename, "wb") as f:
+        shutil.copyfileobj(file.file, f)
+
+    predicted_player = process_video(file_name)
+    player_name = f"{predicted_player.split('_')[0].capitalize()} {predicted_player.split('_')[1].capitalize()}"
     
-    # Generate a unique temporary file name
-    unique_id = uuid.uuid4().hex
-    temp_dir = tempfile.gettempdir()
-    temp_video_path = os.path.join(temp_dir, f"{unique_id}.mp4")
-    
-    try:
-        # Save the uploaded file to the temporary directory
-        with open(temp_video_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-        
-        # Process the video and get the predicted player name
-        predicted_player = process_video(temp_video_path)
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred during processing: {e}")
-    
-    finally:
-        # Clean up: remove the temporary video file
-        if os.path.exists(temp_video_path):
-            os.remove(temp_video_path)
+    # Clean up: remove the temporary video file
+    if os.path.exists(file.filename):
+        os.remove(file.filename)
     
     # Prepare and return the response
     response = {
-        "player": predicted_player,
-        "file_name": file.filename
+        "player": player_name,
+        "file_name": f"{predicted_player}.mp4",
     }
     
     return JSONResponse(content=response)
 
+@app.get("/download/{file_name}")
+def download(file_name: str):
+    if os.path.exists(f"athlete_videos_processed/{file_name}"):
+        return FileResponse(path=f"athlete_videos_processed/{file_name}", media_type="video/mp4", filename=file_name)
+    return JSONResponse(content={"message": "File not found"}, status_code=404)
