fbc77479f04ee866a61cafcd9e19eac24bae5f95
Brennen Ho <76419189+brennenho@users.noreply.github.com>
2024-10-12 23:26:04 -0700

    updated endpoints and readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ## Installation
 
-`python3 -m venv venv`
-`source venv/bin/activate`
-`pip install -r requirements.txt`
+- `python3 -m venv venv`
+- `source venv/bin/activate`
+- `pip install -r requirements.txt`
+
+## Run Server
+
+`fastapi dev main.py`
+
+Server will be run on [http://localhost:3000](http://localhost:3000)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 import shutil
 
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, JSONResponse
 
 app = FastAPI()
 
@@ -23,4 +25,15 @@ def process(file: UploadFile = File(...)):
     with open(file_name, "wb") as f:
         shutil.copyfileobj(file.file, f)
     
-    return {"message": f"File {file_name} uploaded"}
+    response = {
+        "player": "Test Player",
+        "file_name": "test_player.mp4",
+    }
+    
+    return JSONResponse(content=response)
+
+@app.get("/download/{file_name}")
+def download(file_name: str):
+    if os.path.exists(file_name):
+        return FileResponse(path=file_name, media_type="video/mp4", filename=file_name)
+    return JSONResponse(content={"message": "File not found"}, status_code=404)
